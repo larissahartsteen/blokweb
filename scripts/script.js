cbd2707ec260b41180faa4d7da4b49c403d992a3
larissahartsteen <94127726+larissahartsteen@users.noreply.github.com>
2021-11-23 21:23:56 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-// JavaScript Document
+var bekijkMeerButton = document.querySelector("article:nth-of-type(3) button");
+
+bekijkMeerButton.addEventListener("click", verplaatsDeSections);
+
+function verplaatsDeSections(){
+    let hetDerdeArticle = document.querySelector("article:nth-of-type(3)");
+    hetDerdeArticle.classList.toggle("anders");
+}  
